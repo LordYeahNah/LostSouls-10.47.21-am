1429366aef367e801ad7bb45e14735d57fc9be3e
Keegan Miller <144700841+LordYeahNah@users.noreply.github.com>
2023-10-22 16:38:41 +1100

    code clean up

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform _Target;                 // reference to the transform to follow
-    [SerializeField] private float _LerpSpeed;
+    [SerializeField] private float _LerpSpeed;                      // Speed the character will lerp ay
 
     [Header("Camera Bounds")] 
     [SerializeField] private float _MinY;
@@ -25,6 +25,7 @@ public class CameraController : MonoBehaviour
         if (!_Target)
             return;
 
+        // Get the position we want to move to
         Vector3 targetPosition = new Vector3
         {
             x = _Target.transform.position.x,
@@ -32,13 +33,15 @@ public class CameraController : MonoBehaviour
             z = this.transform.position.z
         };
 
+        // Create the lerp
         Vector3 lerpedPosition = Vector3.Slerp(this.transform.position, targetPosition, _LerpSpeed * Time.deltaTime);
+        // Clamp within the camera bounds
         Vector3 clampedPosition = new Vector3
         {
             x = Mathf.Clamp(lerpedPosition.x, _MinX, _MaxX),
             y = Mathf.Clamp(lerpedPosition.y, _MinY, _MaxY),
             z = lerpedPosition.z
         };
-        this.transform.position = clampedPosition;
+        this.transform.position = clampedPosition;                  // Clamp the position
     }
 }
