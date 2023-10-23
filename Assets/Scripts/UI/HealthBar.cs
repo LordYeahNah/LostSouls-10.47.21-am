9c6908cb3d4b0eae1f6adcf90a17220469e258a3
Keegan Miller <Keegan.Miller@eilexinteractive.com>
2023-10-24 10:57:06 +1100

    Added script to control the health bar

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Image = UnityEngine.UI.Image;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private PlayerController _Player;
+    [SerializeField] private Image _HealthSlider;
+
+    private void Awake()
+    {
+        _Player = GameObject.FindGameObjectWithTag("Player")?.GetComponent<PlayerController>();
+        if(_Player)
+            _Player._CharacterTakeDamage.AddListener(UpdateHealthBar);
+        
+        UpdateHealthBar();
+    }
+
+    public void UpdateHealthBar()
+    {
+        if (_Player)
+        {
+            float currentHealth = _Player.CurrentHealth;
+            float percentage = currentHealth / 100;
+            if (_HealthSlider)
+                _HealthSlider.fillAmount = percentage;
+        }
+    }
+}
