91074796d1e904c33365046ddb90f17c0333d73b
Keegan Miller <Keegan.Miller@eilexinteractive.com>
2023-10-25 12:15:36 +1100

    Created a controller for the level

---
+++ Assets/Scripts/Levels/BaseLevelController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseLevelController : MonoBehaviour
+{
+    [Header("Spawn Settings")]
+    [SerializeField] private Transform _PlayerSpawnPoint;                   // Reference to where the player should spawn at
+
+    private void Awake()
+    {
+        SpawnPlayer();
+    }
+
+
+    private void SpawnPlayer()
+    {
+        if (_PlayerSpawnPoint)
+        {
+            GameObject spawnedPlayer = Instantiate(GameController.Instance.PlayerObject);
+            if (spawnedPlayer)
+                spawnedPlayer.transform.position = _PlayerSpawnPoint.transform.position;
+        }
+    }
+}
