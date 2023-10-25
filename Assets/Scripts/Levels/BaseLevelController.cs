3214623c6cb5ae479fa9d447e69f92435ca89250
Keegan Miller <Keegan.Miller@eilexinteractive.com>
2023-10-25 12:18:08 +1100

    Made static for the level

--- Assets/Scripts/Levels/BaseLevelController.cs
+++ Assets/Scripts/Levels/BaseLevelController.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class BaseLevelController : MonoBehaviour
 {
+    public static BaseLevelController Instance;
+    
     [Header("Spawn Settings")]
     [SerializeField] private Transform _PlayerSpawnPoint;                   // Reference to where the player should spawn at
 
     private void Awake()
     {
+        if (!Instance)
+            Instance = this;
+        else 
+            DestroyImmediate(this.gameObject);
+        
         SpawnPlayer();
     }
 
