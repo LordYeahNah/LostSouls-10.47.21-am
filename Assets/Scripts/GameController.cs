a48531a9d87ed7d876752db7d2ce3e33e54213ed
Keegan Miller <Keegan.Miller@eilexinteractive.com>
2023-10-25 12:15:22 +1100

    Added global game controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance;
+
+    public GameObject PlayerObject;                     // Reference to the players character
+
+    private void Awake()
+    {
+        if (!Instance)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            DestroyImmediate(this.gameObject);
+        }
+    }
+}
